57f3139b6ce8a6dab6493b90ecb8ba8514c8cb6b
sugambarithiga <30292330+sugambarithiga@users.noreply.github.com>
2017-07-20 18:04:44 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,21 @@
+    #include<stdio.h>
+    int main()
+    {
+    	int num;
+    	printf("enter the number");
+    	scanf("%d",&num);
+    	if(num>0)
+    	{
+    		printf("the num is positive");
+     
+    	}
+    	else if(num<0)
+    	{
+    		printf("the num is negative");
+     
+    	}
+    	else
+    	{
+    		printf("neighter positive or negative");
+    	}
+    }
